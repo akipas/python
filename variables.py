4c156164af6d1585936200d6d7955f9578d9e5fb
akipas <akimanapascaline@gmail.com>
2020-06-29 10:57:29 +0200

    variables dans python

---
+++ variables.py
@@ -0,0 +1,13 @@
+#python n'a pas besoin de preciser le type d'une variable
+
+a = 52 #declaration d'un entier
+print (f"{a} is an integer")
+
+b = 2.3
+print(f"{b} is a float")
+
+c = True
+print(f"{c} is a boolean")
+
+d = None #a special type with only one possible value(NONE)
+print(f"{d} is a none variable")
